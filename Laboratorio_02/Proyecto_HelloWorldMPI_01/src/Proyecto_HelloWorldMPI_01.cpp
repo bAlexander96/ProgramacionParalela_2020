7fcef0942be078b606e0996d5fee69c2f7638b44
bAlexander96 <berney_14_19@hotmail.com>
2020-01-26 07:31:10 +0100

    añadiendo trabajo

---
+++ Laboratorio_02/Proyecto_HelloWorldMPI_01/src/Proyecto_HelloWorldMPI_01.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Proyecto_HelloWorldMPI_01.c
+ Author      : Alexander
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Compute Pi in MPI C++
+ ============================================================================
+ */
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char *argv[])
+{
+    cout<<"¡Hola Mundo soy proceso unico! "<<endl;
+
+    return 0;
+}
